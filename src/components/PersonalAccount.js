1b62cdcd540905e114e48093e866a2250a56abc5
Demianight <komarovdemian@yandex.ru>
2024-12-17 22:49:05 +0300

    добавил кнопку отмены брони и переадресацию при незалогиненом переходе

--- src/components/PersonalAccount.js
+++ src/components/PersonalAccount.js
@@ -1,37 +1,47 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-import { get } from "../services/apiService";
+import { del, get } from "../services/apiService";
+import { useNavigate } from "react-router-dom";
 
 const PersonalAccount = () => {
-  const [userData, setUserData] = useState(null); // State to store user data
-  const [loading, setLoading] = useState(true); // State to handle loading
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Fetch data from /users/me
     const fetchData = async () => {
-      try {
-        const response = await get("/users/me");
-        if (!response.status === 200)
-          throw new Error("Failed to fetch user data");
-        const data = response.data;
-        setUserData(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
+      get("/users/me")
+        .then((response) => {
+          const data = response.data;
+          setUserData(data);
+        })
+        .catch((error) => {
+          if (error.response.status === 401 || error.response.status === 403) {
+            navigate("/login");
+          }
+        })
+        .finally(() => {
+          setLoading(false);
+        });
     };
 
     fetchData();
   }, []);
 
   if (loading) {
-    return <div className="container">Загрузка...</div>; // Loading indicator
+    return <div className="container">Загрузка...</div>;
   }
 
   if (!userData || !userData.seats || userData.seats.length === 0) {
-    return <div className="container">Нет забронированных мест</div>; // No bookings
+    return <div className="container">Нет забронированных мест</div>;
   }
+  const Deletseat = (seat) => {
+    del(`/seats/${seat.id}/cancel_booking`).then((response) => {
+      if (response.status === 204) {
+        window.location.reload();
+      }
+    });
+  };
 
   return (
     <div className="container">
@@ -39,9 +49,12 @@ const PersonalAccount = () => {
       {userData.seats.map((seat, index) => (
         <div className="card" key={index}>
           <div className="row">
-            <span>Ряд: {seat.row || "Не указан"}</span>
-            <span> Место: {seat.column || "Не указано"}</span>
+            <span>Ряд: {seat.row + 1 || "Не указан"}</span>
+            <span> Место: {seat.column + 1 || "Не указано"}</span>
           </div>
+          <button type="button" onClick={() => Deletseat(seat)}>
+            Отменить
+          </button>
         </div>
       ))}
     </div>
